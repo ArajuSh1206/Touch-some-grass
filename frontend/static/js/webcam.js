0189322088f8c0e9f133c8d7ec04bdafb1a2ad07
AriiSharma <ariisharma47@gmail.com>
2025-07-30 14:19:40 -0600

    added backend server to render route

--- frontend/static/js/webcam.js
+++ frontend/static/js/webcam.js
@@ -67,7 +67,8 @@ function captureAndSend() {
     const formData = new FormData();
     formData.append('file', blob, 'webcam.jpg');
 
-    fetch('http://localhost:5000/predict', {
+    fetch('https://touch-some-grass-backend.onrender.com/predict', // use this on local machine http://localhost:5000/predict 
+      {
         method: 'POST',
         body: formData
     })
